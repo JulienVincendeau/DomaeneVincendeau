29fba6c36d885cef3acabf5853bfe41ec8f87e71
Julien Vincendeau <julien.vincendeau@hism.fr>
2017-03-23 20:39:53 +0100

    version avec echo

---
+++ js/open-video.js
@@ -0,0 +1,22 @@
+/*
+* This is the plugin
+*/
+(function(a){a.createModalVid=function(b){defaults={title:"",message:"Echo",closeButton:true,scrollable:false};var b=a.extend({},defaults,b);var c=(b.scrollable===true)?'style=""':"";html='<div class="modal fade" id="myModal">';html+='<div class="modal-dialog" style="">';html+='<div class="modal-content">';html+='<div class="modal-header">';html+='<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>';if(b.title.length>0){html+='<h4 class="modal-title">'+b.title+"</h4>"}html+="</div>";html+='<div class="modal-body" '+c+">";html+=b.message;html+="</div>";html+='<div class="modal-footer">';if(b.closeButton===true){html+='<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>'}html+="</div>";html+="</div>";html+="</div>";html+="</div>";a("body").prepend(html);a("#myModal").modal().on("hidden.bs.modal",function(){a(this).remove()})}})(jQuery);
+
+/*
+* Here is how you use it for pdf
+*/
+$(function(){
+    $('.view-video').on('click',function(){
+        var pdf_link = $(this).attr('href');
+        //var iframe = '<div class="iframe-container"><iframe src="'+pdf_link+'"></iframe></div>'
+        //var iframe = '<object data="'+pdf_link+'" type="application/pdf"><embed src="'+pdf_link+'" type="application/pdf" /></object>'
+        var iframe = '<iframe width="100%" height="415" src="'+pdf_link+'" frameborder="0" allowfullscreen></iframe>'
+        $.createModalVid({
+            message: iframe,
+            closeButton:false,
+            scrollable:false
+        });
+        return false;
+    });
+})
