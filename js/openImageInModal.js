66acdafdbd3e0370d1957b112ba32383ea533e36
VINCENDEAU Julien <julien.vincendeau@hism.fr>
2018-05-30 23:58:04 +0200

    ajout Domaene Day 2018

---
+++ js/openImageInModal.js
@@ -0,0 +1,7 @@
+// http://jsfiddle.net/6CR2H/1/ pour l'exemple
+$(function() {
+		$('.pop').on('click', function() {
+			$('.imagepreview').attr('src', $(this).find('img').attr('src'));
+			$('#imagemodal').modal('show');
+		});
+});
