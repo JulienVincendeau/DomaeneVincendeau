550e065523fa908f8423d81b6fdba4c8dc72fa63
Julien Vincendeau <julien.vincendeau@hism.fr>
2018-11-14 22:08:22 +0100

    MAJ bootstrap 4.1.3

--- contact.php
+++ contact.php
@@ -100,7 +100,7 @@ if($_POST["submit"]) {
 
       <meta name="description" content="Envie de d&eacute;guster nos vins, nous rencontrer, visiter le dom&auml;ne ? Nous vous accueillons avec plaisir &amp; sur rendez-vous." />
 
-      <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
       <!--Effet du menu -->
       <link rel="stylesheet" type="text/css" href="frameworks\CreativeLinkEffects\css\component.css" />
       <!--Definition des couleurs des barres qui vont tourner autour du lien -->
@@ -307,8 +307,10 @@ if($_POST["submit"]) {
 -->
   </body>
 </html>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 
 <script src="js\mytranslate.js"></script>
 <script src="js\translate.menu.js"></script>
